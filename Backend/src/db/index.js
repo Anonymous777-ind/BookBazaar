b2ff7048f1541986aa67c62717c701719738213c
sany27 <sanyamridhi123@gmail.com>
2024-04-24 18:25:12 +0530

    update index.js

--- Backend/src/db/index.js
+++ Backend/src/db/index.js
@@ -5,6 +5,7 @@ const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
         console.log(`\nMongoDB connected! \nDB Host: ${connectionInstance.connection.host}`);
+        console.log("Doing great job");
     } catch (error) {
         console.log("MongoDB connection error: ", error);
         process.exit(1);
