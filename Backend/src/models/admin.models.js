4427c14b803fe59a9cc591c377171967293d1f0a
sany27 <sanyamridhi123@gmail.com>
2024-04-25 13:57:00 +0530

    admin model created

--- Backend/src/models/admin.models.js
+++ Backend/src/models/admin.models.js
@@ -1,10 +1,47 @@
 import mongoose , {Schema} from 'mongoose'
 
 const adminSchema = new Schema({
-
+    fullname: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    
+    email: {
+        type: String,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        required: true,
+        index: true
+    },
+    password: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    contact: {
+        type: Number,
+        trim: true,
+        required: true,
+        unique: true,
+        max: 10,
+        min: 10
+    },
+    uploads :{
+        type: String,
+        required: true
+    }
+  
     
     
-});
+},
+{
+    timestamps: true
+}
+    
+    
+);
 
 
 module.exports = mongoose.model("admin",adminSchema);
