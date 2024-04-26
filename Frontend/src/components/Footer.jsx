0c0ac81b1ff16be5ce13bae477c1b4eabe833661
sany27 <sanyamridhi123@gmail.com>
2024-04-26 12:49:04 +0530

    Files Updated

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -3,15 +3,29 @@ import React from 'react'
 function Footer() {
   return (
     <>
-    <div className='bg-custom-blue p-14 flex gap-44 	' >
-      <div className="text-custom-white p-8 ml-10 leading-6	text-2xl ">
+    <div className='bg-custom-blue p-14  	' >
+      <div className="flex gap-44">
+      <div className="text-custom-white p-8 ml-8 leading-6	text-2xl  text-white">
         <h1 className='font-custom-heading'>Book Information ?</h1>
-        <p className='text-base leading-'>Please Send us an email at sanyam.sharma.7203@gmail.com</p>
+        <p className='text-base leading-8 font-custom-sub-heading-para'>Please Send us an email at sanyam.sharma.7203@gmail.com</p>
         </div>
-      <div className="text-custom-white p-8 ml-40 leading-6	text-2xl font-custom-heading">
+      <div className="text-custom-white p-8 ml-16 leading-6	text-2xl font-custom-heading text-white">
       <h1 className='font-custom-heading' >Need Help?</h1>
-      <p className='text-base leading-8'>Please call us at +91 9876543210</p>
+      <p className='text-base leading-8 font-custom-sub-heading-para '>Please call us at +91 9876543210</p>
       </div>
+
+
+      </div> 
+    <hr />
+   <div className=''>
+    <div className='text-white leading-8 mx-0'>
+      {/* <img src="../public/logo.png" alt="" /> */}
+      <h1>Book Bazzar</h1>
+      </div>
+    <div className='mr-0'>
+      <p className='text-white text-xl custom-sub-heading-para text-center '> @ 2024 All rights reserved. Made with 🤍</p>
+    </div>
+   </div>
     </div>
    
     </>
