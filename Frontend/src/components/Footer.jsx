684d0c8ecd91af155e2f965c95f50195444db929
sany27 <sanyamridhi123@gmail.com>
2024-04-29 13:36:59 +0530

    files modified

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -5,40 +5,47 @@ import { faHandshakeAngle } from '@fortawesome/free-solid-svg-icons'
 
 function Footer() {
   return (
-    <>
-     <footer className='bg-custom-blue p-8 w-screen	'>
-      <div className="footer-top-info w-full lg:flex md:flex gap-24 ">
-      <div className="footer-left flex gap-4">
-      <div className="icon  ">
+    <footer className='bg-custom-blue pt-8 pb-8 w-screen 	'>
+      <section className='w-full container mx-auto p-2'>
+        <div className="footer-top-info w-full flex-row  justify-around pb-8 lg:flex sm:flex">
+          <div className="footer-left flex gap-4">
+            {/* book information icon */}
+            <div className="icon  ">
               <FontAwesomeIcon icon={faLayerGroup} size="2xl" style={{ color: "#5a8f7b" }} />
             </div>
-            <div className="text-custom-white  leading-6	text-2xl flex-col gap-6  text-white">
+            {/* book information */}
+            <div className="text-custom-white  	text-2xl flex-col gap-6  text-white">
               <h1 className='font-custom-heading'>Book Information ?</h1>
-              <p className='text-base leading-6 font-custom-sub-heading-para pt-2'>Please Send us an email at sanyam.sharma.7203@gmail.com</p>
+              <p className='text-base text-wrap font-custom-sub-heading-para pt-2 '>Please Send us an email at support.us@gmail.com</p>
             </div>
-      </div>
-      <div className="footer-right flex gap-4 ">
-      <div className="icon">
-          <FontAwesomeIcon icon={faHandshakeAngle} size="2xl" style={{color: "#5a8f7b"}} />
           </div>
-          <div className="text-custom-white   leading-6	text-2xl flex-col gap-6  text-white">
-            <h1 className='font-custom-heading' >Need Help?</h1>
-            <p className='text-base leading-6 font-custom-sub-heading-para pt-2'>Please call us at +91 9876543210</p>
-          </div>
-      </div>
-      </div>
-
-      <div className="footer-bottom-info">
-      <hr />
-        <div className=' pt-5 '>
-          <div >
-            <p className='text-white text-xl custom-sub-heading-para text-center'> ©  2024 All rights reserved. Made with 🤍</p>
+          <div className="footer-right flex gap-4 ">
+            {/* Need help icon */}
+            <div className="icon">
+              <FontAwesomeIcon icon={faHandshakeAngle} size="2xl" style={{ color: "#5a8f7b" }} />
+            </div>
+            {/* Need Help */}
+            <div className="text-custom-white  	text-2xl flex-col gap-6  text-white">
+              <h1 className='font-custom-heading' >Need Help?</h1>
+              <p className='text-base   font-custom-sub-heading-para pt-2'>Please call us at +91 9876543210</p>
+            </div>
           </div>
         </div>
-      </div>
-     </footer>
+        {/* terms and condition section */}
+        <div className="footer-bottom-info container mx-auto">
+          {/* horizontal line */}
+          <hr className='opacity-35' />
+          <div className=' pt-5 '>
+            <div >
+              <p className='text-white text-xl custom-sub-heading-para text-center'> ©  2024 All rights reserved. Made with 🤍</p>
+            </div>
+          </div>
+        </div>
+      </section>
 
-    </>
+    </footer>
+
+
 
   )
 }
