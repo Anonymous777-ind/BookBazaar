16ff8725b52a42c261e6cac68a5d1b193e966536
sany27 <sanyamridhi123@gmail.com>
2024-04-29 12:27:15 +0530

    files modified

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -5,37 +5,41 @@ import { faHandshakeAngle } from '@fortawesome/free-solid-svg-icons'
 
 function Footer() {
   return (
-      <footer className='bg-custom-blue p-14 mx-auto' >
-        <div className="flex gap-24 container w-full">
-          <div className="p-8 flex gap-4">
-            <div className="icon  ">
+    <>
+     <footer className='bg-custom-blue p-8 w-screen	'>
+      <div className="footer-top-info w-full lg:flex md:flex gap-24 ">
+      <div className="footer-left flex gap-4">
+      <div className="icon  ">
               <FontAwesomeIcon icon={faLayerGroup} size="2xl" style={{ color: "#5a8f7b" }} />
             </div>
-            <div className="text-custom-white  leading-6	text-2xl  text-white">
-              <h1 className='font-custom-heading text-[20px] xs:text-[10px] '>Book Information ?</h1>
-              <p className='text-[16px] leading-8 font-custom-sub-heading-para'>Please Send us an email at sanyam.sharma.7203@gmail.com</p>
+            <div className="text-custom-white  leading-6	text-2xl flex-col gap-6  text-white">
+              <h1 className='font-custom-heading'>Book Information ?</h1>
+              <p className='text-base leading-6 font-custom-sub-heading-para pt-2'>Please Send us an email at sanyam.sharma.7203@gmail.com</p>
             </div>
-          </div>
-          <div className="mt-8 flex gap-4">
-          <div className="icon">
+      </div>
+      <div className="footer-right flex gap-4 ">
+      <div className="icon">
           <FontAwesomeIcon icon={faHandshakeAngle} size="2xl" style={{color: "#5a8f7b"}} />
           </div>
-          <div className="text-custom-white leading-6	text-2xl font-custom-heading text-white">
-            <h1 className='font-custom-heading md:text-[20px] xs:text-[10px]' >Need Help?</h1>
-            <p className='leading-8 font-custom-sub-heading-para md:text-[16px] xs:text-[10px]'>Please call us at +91 9876543210</p>
+          <div className="text-custom-white   leading-6	text-2xl flex-col gap-6  text-white">
+            <h1 className='font-custom-heading' >Need Help?</h1>
+            <p className='text-base leading-6 font-custom-sub-heading-para pt-2'>Please call us at +91 9876543210</p>
           </div>
-          </div>
-          
+      </div>
+      </div>
 
-
-        </div>
-        <hr />
+      <div className="footer-bottom-info">
+      <hr />
         <div className=' pt-5 '>
           <div >
             <p className='text-white text-xl custom-sub-heading-para text-center'> ©  2024 All rights reserved. Made with 🤍</p>
           </div>
         </div>
-      </footer>
+      </div>
+     </footer>
+
+    </>
+
   )
 }
 
--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -18,7 +18,8 @@ export default {
       },
       padding: {
         "padding-sm": "0.875rem"
-      }
+      },
+      
     },
   },
   plugins: [],
