108cfbba9b13caab588e0c654e2836a3c027e492
Aayush Thakur <ayush.thakur.1243@gmail.com>
2024-04-29 14:01:37 +0530

    footer updated

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -7,16 +7,16 @@ function Footer() {
   return (
     <footer className='bg-custom-blue pt-8 pb-8 w-screen 	'>
       <section className='w-full container mx-auto p-2'>
-        <div className="footer-top-info w-full flex-row  justify-around pb-8 lg:flex sm:flex">
+        <div className="footer-top-info w-full flex justify-around pb-8 xs:flex-col gap-6">
           <div className="footer-left flex gap-4">
             {/* book information icon */}
             <div className="icon  ">
               <FontAwesomeIcon icon={faLayerGroup} size="2xl" style={{ color: "#5a8f7b" }} />
             </div>
             {/* book information */}
-            <div className="text-custom-white  	text-2xl flex-col gap-6  text-white">
-              <h1 className='font-custom-heading'>Book Information ?</h1>
-              <p className='text-base text-wrap font-custom-sub-heading-para pt-2 '>Please Send us an email at support.us@gmail.com</p>
+            <div className="text-custom-white text-white">
+              <h1 className='font-custom-heading text-[24px] xs:text-[20px]'>Book Information ?</h1>
+              <p className='text-base text-wrap font-custom-sub-heading-para pt-2 xs:text-[14px]'>Please Send us an email at support.us@gmail.com</p>
             </div>
           </div>
           <div className="footer-right flex gap-4 ">
@@ -25,9 +25,9 @@ function Footer() {
               <FontAwesomeIcon icon={faHandshakeAngle} size="2xl" style={{ color: "#5a8f7b" }} />
             </div>
             {/* Need Help */}
-            <div className="text-custom-white  	text-2xl flex-col gap-6  text-white">
-              <h1 className='font-custom-heading' >Need Help?</h1>
-              <p className='text-base   font-custom-sub-heading-para pt-2'>Please call us at +91 9876543210</p>
+            <div className="text-custom-white text-white">
+              <h1 className='font-custom-heading text-[24px] xs:text-[20px]' >Need Help?</h1>
+              <p className='text-base font-custom-sub-heading-para pt-2 xs:text-[14px]'>Please call us at +91 9876543210</p>
             </div>
           </div>
         </div>
@@ -37,7 +37,7 @@ function Footer() {
           <hr className='opacity-35' />
           <div className=' pt-5 '>
             <div >
-              <p className='text-white text-xl custom-sub-heading-para text-center'> ©  2024 All rights reserved. Made with 🤍</p>
+              <p className='text-white text-sm custom-sub-heading-para text-center'> ©  2024 All rights reserved. Made with 🤍</p>
             </div>
           </div>
         </div>
