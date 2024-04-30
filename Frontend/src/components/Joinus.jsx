65d7c79a0150ef864318a1f946cbea2ac6f79777
sany27 <sanyamridhi123@gmail.com>
2024-04-30 13:00:05 +0530

    Image Uploaded

---
+++ Frontend/src/components/Joinus.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+function Joinus() {
+  return (
+   <section>
+    <div className="inner">
+        <div className="left-inner">
+         
+        </div>
+        <div className="right-inner">
+
+        </div>
+    </div>
+   </section>
+  )
+}
+
+export default Joinus
--- Frontend/src/components/Layout.jsx
+++ Frontend/src/components/Layout.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import Carousel from "./Carousel/Carousel";
+import {Outlet} from "react-router-dom"
+import Joinus from "./Joinus";
 
 function Layout() {
   return (
     <>
       <Header />
       <Carousel /> 
+      {/* <Joinus/> */}
+     <Outlet/> 
        <Footer />
     </>
   );
