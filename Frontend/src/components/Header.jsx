4b1f5054b8bec6f48d564fde76b07b814470f31c
Aayush Thakur <ayush.thakur.1243@gmail.com>
2024-04-30 12:15:23 +0530

    File updated

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -5,7 +5,6 @@ function Header() {
   return (
     <header className="w-screen h-28 border-2 border-black xs:px-2">
       <nav className="w-full mx-auto h-full container  border-2 border-red-600">
-        <div><FontAwesomeIcon icon="fa-solid fa-bars" size="xl" style={{color: "#2f4858",}} /></div>
       </nav>
     </header>
   );
