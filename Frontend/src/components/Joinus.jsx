fd32ceb59bfc662052d7a93861fd4b8c9e5fa302
sany27 <sanyamridhi123@gmail.com>
2024-04-30 13:03:36 +0530

    modified

--- Frontend/src/components/Joinus.jsx
+++ Frontend/src/components/Joinus.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import coverImage from "../assets/uploads/"
+import coverImage from "../assets/uploads/cover-1.png"
 
 function Joinus() {
   return (
    <section>
     <div className="inner">
         <div className="left-inner">
-         
+         <img src={coverImage} alt="" />
         </div>
         <div className="right-inner">
 
