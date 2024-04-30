c47cdcfe8b48e9e015bfea6cbd72eab1fbf05acf
sany27 <sanyamridhi123@gmail.com>
2024-04-30 14:00:17 +0530

    modified

--- Frontend/src/components/Joinus.jsx
+++ Frontend/src/components/Joinus.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 
+// import coverImage from "../assets/uploads/cover1.jpg"
 
 function Joinus() {
-  return (
-   <section>
-    <div className="inner">
-        <div className="left-inner">
-       
-        </div>
-        <div className="right-inner">
-          <h1></h1>
-        </div>
-    </div>
-   </section>
-  )
+    return (
+        <section className='container mx-auto'>
+            <div className="inner w-full border-2 border-gray-600 bg-white bg-cover">
+                <div className="bg-coverImage h-auto w-auto bg-cover border-4  border-red-600">
+                    {/* <img className='h-2/6 w-[20%] text-center' src={coverImage} alt="book" /> */}
+                </div>
+                <div className="right-inner">
+                    <h1></h1>
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default Joinus
--- Frontend/src/components/Layout.jsx
+++ Frontend/src/components/Layout.jsx
@@ -10,7 +10,7 @@ function Layout() {
     <>
       <Header />
       <Carousel /> 
-      {/* <Joinus/> */}
+      <Joinus/>
      <Outlet/> 
        <Footer />
     </>
