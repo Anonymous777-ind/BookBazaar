7bd26be04c62bde669c1761dc7744df177e786fe
sany27 <sanyamridhi123@gmail.com>
2024-04-30 13:13:52 +0530

    modified

---
+++ Frontend/src/components/Joinus.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {image} from ""
+
+function Joinus() {
+  return (
+   <section>
+    <div className="inner">
+        <div className="left-inner">
+         <img src={coverImage} alt="" />
+        </div>
+        <div className="right-inner">
+
+        </div>
+    </div>
+   </section>
+  )
+}
+
+export default Joinus
