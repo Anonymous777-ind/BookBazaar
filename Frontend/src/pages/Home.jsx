ce1565795ce881a8f0ee93d41f54fe3df781f818
sany27 <sanyamridhi123@gmail.com>
2024-05-01 16:55:00 +0530

    file modified

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Carousel from '../components/Carousel/Carousel'
+
 import Joinus from '../components/Joinus/Joinus'
+import BookGallery from '../components/Gallery/BookGallery'
 
 function Home() {
   return (
     <>
         <Carousel/>
+        <BookGallery/>
         <Joinus/>
     </>
   )
