b858f70cd732247352186fabc6f5fc4bcd3fe1a4
sany27 <sanyamridhi123@gmail.com>
2024-05-01 17:28:23 +0530

    file modified

--- Frontend/src/components/Gallery/BookGallery.jsx
+++ Frontend/src/components/Gallery/BookGallery.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+
 
 function BookGallery() {
     return (
         <section className='container mx-auto w-full'>
            <div className="left">
-            
+
            </div>
            <div className="right"></div>
 
