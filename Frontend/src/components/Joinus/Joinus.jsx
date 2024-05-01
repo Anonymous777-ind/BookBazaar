9f647ef065633b043fd345db045aff9d1e93726a
sany27 <sanyamridhi123@gmail.com>
2024-05-01 13:54:42 +0530

    file added

--- Frontend/src/components/Joinus/Joinus.jsx
+++ Frontend/src/components/Joinus/Joinus.jsx
@@ -1,20 +1,61 @@
-import React from 'react'
+import React from 'react';
+import './Joinus.css'; // Import CSS file
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-// import coverImage from "../assets/uploads/cover1.jpg"
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 function Joinus() {
     return (
-        <section className='container mx-auto'>
-            <div className="inner w-full border-2 border-gray-600 bg-white bg-cover">
-                <div className="bg-coverImage h-auto w-auto bg-cover border-4  border-red-600">
-                    {/* <img className='h-2/6 w-[20%] text-center' src={coverImage} alt="book" /> */}
+        <section className='container mx-auto my-16  ' >
+           <div className="rounded-md container  mx-auto w-full sm:max-w-3xl lg:max-w-5xl flex flex-col sm:flex-row justify-center lg:justify-between gap-10 shadow-shadow h-auto xs:px-2 xs:py-3">
+                <div className="image-wrapper">
+                    <div className="bgimg "></div>
                 </div>
-                <div className="right-inner">
-                    <h1></h1>
+                <div className="subscribe-content">
+                    <h1 className='font-custom-heading text-3xl font-bold text-custom-blue'>Join Our Community</h1>
+                    <p className='text-custom-gray my-3 text-xl w-full font-semibold font-custom-sub-heading-para'>Sign up & get 10% of on your First Book</p>
+                    <form action="">
+                        <div className="input my-5 xs:flex-col gap-3">
+                            <input className=' text-custom-gray font-custom-sub-heading-para leading-normal' type="mail" placeholder='Your email' required />
+                            <button className='btn text-white font-custom-sub-heading-para text-lg px-10 bg-custom-green  xs:rounded-full xs:inline-block xs:text-lg xs:text-center xs:mx-auto ' type="submit">Subscribe</button>
+                        </div>
+                       
+                    </form>
+                    <ul className='social-icons'>
+                        <li style={{ backgroundColor: '#5a8f7b' }}>
+                            <button onClick={() => window.open("https://www.facebook.com", "_blank")}>
+                            <FontAwesomeIcon icon={faFacebookF} size="xl" style={{color: "#ffffff",}} />
+                            </button>
+                        </li>
+                        <li style={{ backgroundColor: '#5a8f7b' }}>
+                            <button onClick={() => window.open("https://www.twitter.com", "_blank")}>
+                                <FontAwesomeIcon icon={faTwitter} size="xl" style={{ color: "#ffffff" }} />
+                            </button>
+                        </li>
+                        <li style={{ backgroundColor: '#5a8f7b' }}>
+                            <button onClick={() => window.open("https://www.instagram.com", "_blank")}>
+                                <FontAwesomeIcon icon={faInstagram} size="xl" style={{ color: "#ffffff" }} />
+                            </button>
+                        </li>
+                        <li style={{ backgroundColor: '#5a8f7b' }}>
+                            <button onClick={() => window.open("https://www.linkedin.com", "_blank")}>
+                                <FontAwesomeIcon icon={faLinkedin} size="xl" style={{ color: "#ffffff" }} />
+                            </button>
+                        </li>
+                        <li style={{ backgroundColor: '#5a8f7b' }}>
+                            <button onClick={() => window.open("https://www.youtube.com", "_blank")}>
+                                <FontAwesomeIcon icon={faYoutube} size="lg" style={{ color: "#ffffff" }} />
+                            </button>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </section>
-    )
+    );
 }
 
-export default Joinus
+export default Joinus;
